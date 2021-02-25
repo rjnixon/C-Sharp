bedb76df93aa3e2f53ba70225bc05ba17c934ecc
rjnixon <65729042+rjnixon@users.noreply.github.com>
2021-02-25 15:00:09 -0500

    Week 7 lab

---
+++ NixonWeek7Lab/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NixonWeek6Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            //initializing PersonV2 class
+            PersonV2 temp = new PersonV2();
+            //Assigning values
+            temp.Feedback = "";
+            temp.FName = txtFirstName.Text;
+            temp.MName = txtMiddleName.Text;
+            temp.LName = txtLastName.Text;
+            temp.Street1 = txtStreet1.Text;
+            temp.Street2 = txtStreet2.Text;
+            temp.City = txtCity.Text;
+            temp.State = txtState.Text;
+            temp.ZipCode = txtZipCode.Text;
+            temp.PhoneNum = txtPhoneNum.Text;
+            temp.CellPhone = txtCellPhone.Text;
+            temp.Email = txtEmail.Text;
+            temp.InstagramLink = txtInstagramLink.Text;
+
+            //if input is wrong it displays error
+            if (temp.Feedback.Contains("ERROR:"))
+            {
+                labelFeedback.Text = temp.Feedback;
+
+                //If input is correct  values are displayed
+            } else
+            {
+                //labelFeedback.Text = "Information Inputted:\nFirst Name: " + temp.FName + "\nMiddle Name: " + temp.MName + "\nLast Name: " + temp.LName + "\nStreet 1: " + temp.Street1 + "\nStreet 2: " + temp.Street2 + "\nCity: " + temp.City + "\nState: " + temp.State + "\nZip Code: " + temp.ZipCode + "\nHome Phone #: " + temp.PhoneNum + "\nCell Phone #:" + temp.CellPhone + "\nEmail: " + temp.Email + "\nInstagram Username: " + temp.InstagramLink;
+                //labelFeedback.Text = temp.Feedback;
+                string strFeedback = temp.AddARecord();
+                labelFeedback.Text = strFeedback;
+            }
+        }
+    }
+}
---
+++ NixonWeek7Lab/PersonV2.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace NixonWeek6Lab
+{
+    //personv2 class inheriting person class
+    class PersonV2 : Person
+    {
+        //initializing
+        private string cellPhone;
+        private string instagramLink;
+
+        //get set cellhpone
+        public string CellPhone
+        {
+            get
+            {
+                return cellPhone;
+            }
+
+            set
+            {
+                //validating information
+                if (Validations.IsPhoneNumValid(value))
+                {
+                    cellPhone = value;
+                } else
+                {
+                    Feedback += "\nERROR: " + "Cell Phone # is incorrect.";
+                }
+            }
+        }
+
+        //get set instagram username
+        public string InstagramLink
+        {
+            get
+            {
+                return instagramLink;
+            }
+
+            set
+            {
+                //validating information
+                if (Validations.IsInstagramLinkValid(value))
+                {
+                    instagramLink = value;
+                } 
+                else
+                {
+                    Feedback += "\nERROR: " + "Instagram Link is incorrect.";
+                }
+            }
+        }
+
+        public string AddARecord()
+        {
+            string strResult = "";
+
+            //Initialize connection
+            SqlConnection Conn = new SqlConnection();
+
+            //Sent connection string via Discord
+            Conn.ConnectionString = 
+
+            string strSQL = "INSERT INTO PersonV2 (FName, MName, LName, Street1, Street2, City, State, ZipCode, PhoneNum, Email, CellPhone, InstagramLink) VALUES (@FName, @MName, @LName, @Street1, @Street2, @City, @State, @ZipCode, @PhoneNum, @Email, @CellPhone, @InstagramLink)";
+
+            //command
+            SqlCommand comm = new SqlCommand();
+            comm.CommandText = strSQL;
+            comm.Connection = Conn; //connection
+
+            //Filling in values for parameters
+            comm.Parameters.AddWithValue("@FName", FName);
+            comm.Parameters.AddWithValue("@MName", MName);
+            comm.Parameters.AddWithValue("@LName", LName);
+            comm.Parameters.AddWithValue("@Street1", Street1);
+            comm.Parameters.AddWithValue("@Street2", Street2);
+            comm.Parameters.AddWithValue("@City", City);
+            comm.Parameters.AddWithValue("@State", State);
+            comm.Parameters.AddWithValue("@ZipCode", ZipCode);
+            comm.Parameters.AddWithValue("@PhoneNum", PhoneNum);
+            comm.Parameters.AddWithValue("@Email", Email);
+            comm.Parameters.AddWithValue("@CellPhone", CellPhone);
+            comm.Parameters.AddWithValue("@InstagramLink", InstagramLink);
+
+            //trying to connect to server
+            try
+            {
+                //open connection
+                Conn.Open();
+                //number of records inputted
+                int intRecs = comm.ExecuteNonQuery();
+                //output if records successfully added
+                strResult = $"Connected: Inserted {intRecs} records.";
+                //close connection
+                Conn.Close();
+            }
+            catch (Exception err)
+            {
+                //Error output if something goes wrong with connection
+                strResult = "ERROR:" + err.Message;
+            }
+            finally
+            {
+
+            }
+
+            return strResult;
+        }
+        //constructor 
+        public PersonV2(): base()
+        {
+            cellPhone = "";
+            instagramLink = "";
+        }
+    }
+}
