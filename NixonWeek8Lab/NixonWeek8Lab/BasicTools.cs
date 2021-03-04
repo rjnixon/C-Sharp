eebf9d907e3765e57135e1a352e8106ee7b2d97c
rjnixon <65729042+rjnixon@users.noreply.github.com>
2021-03-04 12:13:43 -0500

    Add files via upload

---
+++ NixonWeek8Lab/NixonWeek8Lab/BasicTools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NixonWeek6Lab
+{
+    //Basic Tools Class
+    public class BasicTools
+    {
+
+        //Pause Function
+        public static void Pause()
+        {
+            Console.WriteLine("\nPress any key to Continue\n");
+            Console.ReadKey();
+        }
+    }
+}
